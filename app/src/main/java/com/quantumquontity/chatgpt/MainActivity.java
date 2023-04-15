29ad8896a4f7530d80b97b077f9768df02e86c1b
Mykhailo Zablotskyi <mykhailo.zablotskyi@custodiatechnology.com>
2023-04-15 14:13:40 +0300

    Сделал приложение на весь экран, и что меню не заходило на status bar

--- app/src/main/java/com/quantumquontity/chatgpt/MainActivity.java
+++ app/src/main/java/com/quantumquontity/chatgpt/MainActivity.java
@@ -146,7 +146,8 @@ public class MainActivity extends AppCompatActivity {
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        getWindow().setStatusBarColor(getColor(R.color.background));
+        getWindow().setNavigationBarColor(getColor(R.color.background));
 
         findElement();
         initPages();
